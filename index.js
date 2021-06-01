76fb005f9a517b17ecda9d3fc7f23908762b6e03
samanthadotdesign <samleeml@gmail.com>
2021-06-01 14:28:34 +0800

    icstyling changes and included About page

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import pg from 'pg';
 import express from 'express';
+import methodOverride from 'method-override';
+import cookieParser from 'cookie-parser';
 
 /* ============ CONFIGURATION =========== */
 
@@ -12,6 +14,15 @@ app.set('views', './views');
 
 // Set up static directory
 app.use(express.static('public'));
+
+// Receive POST Requests in Express
+app.use(express.urlencoded({ extended: true }));
+
+// Override POST requests with query param ?_method=PUT to be PUT requeåsts
+app.use(methodOverride('_method'));
+
+// Cookies to save user id for updating skills & SQL table user_skills
+app.use(cookieParser());
 
 // Set the way we will connect to the server
 const pgConnectionConfigs = {
@@ -29,16 +40,16 @@ pool.connect();
 
 /* ============ DASHBOARD =========== */
 
-app.get('/', async (req, res)=> {
-  const listSectionsQuery = `SELECT * FROM sections`;
-  const listCategoriesQuery = `SELECT * FROM categories`;
-  const listSkillsQuery = `SELECT * FROM skills`;
-  const listResourcesQuery = `SELECT * FROM resources`;
+app.get('/', async (req, res) => {
+  const listSectionsQuery = 'SELECT * FROM sections';
+  const listCategoriesQuery = 'SELECT * FROM categories';
+  const listSkillsQuery = 'SELECT * FROM skills';
+  const listResourcesQuery = 'SELECT * FROM resources';
 
-    try {
+  try {
     const listSectionsRes = await pool.query(listSectionsQuery);
     const sectionsArr = listSectionsRes.rows;
-    
+
     const listCategoriesRes = await pool.query(listCategoriesQuery);
     const categoriesArr = listCategoriesRes.rows;
 
@@ -48,11 +59,59 @@ app.get('/', async (req, res)=> {
     const listResourcesRes = await pool.query(listResourcesQuery);
     const resourcesArr = listResourcesRes.rows;
 
-    res.render('dashboard', { sections: sectionsArr, categories: categoriesArr, skills: skillsArr, resources: resourcesArr });
-
+    res.render('dashboard', {
+      sections: sectionsArr, categories: categoriesArr, skills: skillsArr, resources: resourcesArr,
+    });
   } catch (err) {
-    console.log(err.stack)
+    console.log(err.stack);
   }
+});
+
+/* ============ ACTIONS INSIDE DASHBOARD =========== */
+
+/**
+ * When user clicks "Complete Skill" inside div.resources
+ * Updates SQL table user_skills
+ */
+app.put('/complete-skill/:id', async (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.cookies;
+
+  const markSkillCompleteQuery = `UPDATE user_skills SET skill_completed=true WHERE user_id=${userId} AND skill_id=${id}`;
+
+  try {
+    await pool.query(markSkillCompleteQuery);
+    res.redirect('/');
+  } catch (err) {
+    console.log(err.stack);
+  }
+});
+
+/**
+ * When user clicks "Uncomplete Skill" inside div.resources
+ * Updates SQL table user_skills
+ */
+app.put('/uncomplete-skill/:id', async (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.cookies;
+
+  const markSkillUncompleteQuery = `UPDATE user_skills SET skill_completed=false WHERE user_id=${userId} AND skill_id=${id}`;
+
+  try {
+    await pool.query(markSkillUncompleteQuery);
+    console.log('done');
+    res.redirect('/');
+  } catch (err) {
+    console.log(err.stack);
+  }
+});
+
+/* ============ LOGIN =========== */
+
+/* ============ CONTRIBUTE RESOURCES =========== */
+
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 /* ============ LISTEN =========== */
